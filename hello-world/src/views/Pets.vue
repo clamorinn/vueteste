72f081c519f48ea2572cf88ae8ad9a6ef1a15a37
DanielCorreia <dhac@cin.ufpe.br>
2020-01-10 19:16:51 -0300

    ...

---
+++ hello-world/src/views/Pets.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1> MEUS PETS QUERIDOS </h1>
+    </div>
+
+   
+</template>
+
+ <style>
+
+        h1{
+            color:pink;
+            font-size: 100px;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            display: flex;
+            justify-content: left;
+            align-items:center;
+            align-self: center;
+        }
+
+
+    </style>
